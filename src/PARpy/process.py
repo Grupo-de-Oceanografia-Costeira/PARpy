a32f59934787bc2579f69f76125aa12fcfa42666
arnaldorusso <arnaldorusso@gmail.com>
2013-09-16 17:26:13 -0300

    parsing par values and dates in a list of dicts

--- src/PARpy/process.py
+++ src/PARpy/process.py
@@ -6,15 +6,11 @@ from datetime import datetime
 import glob
 import os
 
-<<<<<<< HEAD
+
 def extract_mat(indir):
     dicts = []
     d = {}
-=======
-hours_proc = []
 
-def extract_mat(indir):
->>>>>>> ecda31fa4351999aa5a0a7811d0523e9a1f8d7a9
     for filename in sorted(glob.glob(os.path.join(indir, '*.mat'))):
         f = loadmat(filename)
         dat = f['hdfdata']['Reference_Par_hyperspectral_data']
@@ -32,13 +28,6 @@ def extract_mat(indir):
             if len(str(hour)) == 3:
                 d['hours'].append(datetime.strptime(str(hour)+'0', "%H%M%S%f"))
             else:
-<<<<<<< HEAD
                 d['hours'].append(datetime.strptime(str(hour), "%H%M%S%f"))
         
     return dicts
-
-=======
-                hours_proc.append(datetime.strptime(str(i), "%H%M%S%f"))
-    
-    return par, dates, hours 
->>>>>>> ecda31fa4351999aa5a0a7811d0523e9a1f8d7a9
