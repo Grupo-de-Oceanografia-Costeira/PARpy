c1b52254d73aeb6506b7a685063c57c8303f694a
arnaldorusso <arnaldorusso@gmail.com>
2013-09-16 17:41:36 -0300

    fix number of files

--- src/PARpy/process.py
+++ src/PARpy/process.py
@@ -13,21 +13,26 @@ def extract_mat(indir):
 
     for filename in sorted(glob.glob(os.path.join(indir, '*.mat'))):
         f = loadmat(filename)
+        print filename
         dat = f['hdfdata']['Reference_Par_hyperspectral_data']
         par = dat[0,0][:,2]
         dates = dat[0,0][:,0]
         hours = np.int64(dat[0,0][:,0])
-        if d:
-            dicts.append(d)
-        d = {}
+
         d['name'] = filename
         d['par'] = par
         d['dates'] = dates
-        d['hours'] = [] 
+        d['hours'] = []
+
         for hour in hours:
             if len(str(hour)) == 3:
                 d['hours'].append(datetime.strptime(str(hour)+'0', "%H%M%S%f"))
             else:
                 d['hours'].append(datetime.strptime(str(hour), "%H%M%S%f"))
+
+        if d:
+            dicts.append(d)
+        d = {}
+
         
     return dicts
