c5e5a2696f2e1e609713b4dec1d87622b4ae3209
arnaldorusso <arnaldorusso@gmail.com>
2013-09-27 15:02:42 +0200

    optim

--- PARpy/sun_filter.py
+++ PARpy/sun_filter.py
@@ -2,22 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import Pysolar
+from datetime import datetime
 
-def filter_solar_angle(Value, Lat, Lon, Temp):
+def filter_solar_angle(Lat, Lon, Value, Temp):
     '''
-    Input:
-    ------
-    Value: An array of datetime objects
-    Lat: Latitude in degrees
-    Lon: Longitude in degrees
-    temp: Temperature of air
+    Filter values of an array by their valid solar angle.
+    Satlantic HyperOCR Hyperspectral Radiometer.
+    http://satlantic.com/hyperspectral-radiometers
     
-    '''
-    filt = []
-    
-    for h in value:
-        ang = Pysolar.GetAltitude(Lat, Lon, h, temperature_celsius=Temp)
-        if ang > 30:
-            filt.append[h]
+    Pysolar is used to calculate elevation angles greater than 30 deg.
         
+    Parameters
+    ----------
+    Lat : Latitude in degrees
+    Lon : Longitude in degrees
+    Value : An array of datetime objects
+    temp : Temperature of air
+    
+    Returns
+    -------
+    
+    temp : ndarray of boolean values
+    filt : Filtered values
+    '''
+    
+    filt = []
+    temp = []
+    
+    for h in Value:
+        if type(h) != datetime:
+            temp.append(np.nan)
+        else:
+            ang = Pysolar.GetAltitude(Lat, Lon, h, temperature_celsius=Temp)
+            temp.append(ang)
+            print ang
+    
+    temp = np.array(temp)
+    Value = np.array(Value)    
+    filt = Value[temp>30]    
+    
+    return temp,filt
     
